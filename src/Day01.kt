4721a660522f3b8fa0e9e6eb6f731aa778318f8a
Uipko Berghuis <uipko.berghuis@gmail.com>
2022-12-01 20:07:59 +0100

    Day01 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,19 +2,29 @@ import kotlin.math.max
 
 fun main() {
     part1("Day01")
-
+    part2("Day01")
 }
 
 fun part1(fileName: String) {
-    var maxCalories = 0
+    val maxCalories = inventories(fileName).maxOf { it }
+    println("The maximum number of Calories in an Elf's inventory is $maxCalories")
+}
+
+fun part2(fileName: String) {
+    val topThreeInventories = inventories(fileName).sortedDescending().take(3).sum()
+    println("The total Calories of the top three inventories is $topThreeInventories")
+}
+
+fun inventories(fileName: String ): List<Int> {
+    val inventories = mutableListOf<Int>()
     var sumOfCalories = 0
     for (line in readInput(fileName)) {
         if (line.isEmpty()) {
-            maxCalories = max(maxCalories, sumOfCalories)
+            inventories.add(sumOfCalories)
             sumOfCalories = 0
         } else {
             sumOfCalories += line.toInt()
         }
     }
-    println("The maximum number of Calories in an Elf's inventory is $maxCalories")
+    return inventories
 }
