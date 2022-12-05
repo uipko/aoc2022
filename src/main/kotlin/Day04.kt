ed790b6219935db199fc27c0ff1c7128f92d7a93
Uipko Berghuis <uipko.berghuis@gmail.com>
2022-12-05 21:11:42 +0100

    Day04 part1 & part 2
    TODO: cleanup code, use more function chaining
    Also add total over real list to tests

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -7,6 +7,7 @@ fun main() {
     println("The total sum of pairs of elf to reconsider is: $pairsOverlap")
 }
 
+
 fun contains(fileName: String): Int {
     val test = readInput(fileName).count { pairs ->
         val elfRange = pairs.split(",").map {
@@ -14,12 +15,20 @@ fun contains(fileName: String): Int {
         val elf1 = elfRange.first()
         val elf2 = elfRange.last()
         (elf1.first() >= elf2.first() && elf1.last() <= elf2.last())
-        || (elf2.first() >= elf1.first() && elf2.last() <= elf1.last())
+                || (elf2.first() >= elf1.first() && elf2.last() <= elf1.last())
     }
     return test
 }
 
 fun overlap(fileName: String): Int {
+    return readInput(fileName).count { pairs ->
+        pairs.split(",").map {
+            range -> range.split("-").map { it.toInt() }.toSet()
+        }.reduce { acc, next -> acc.intersect(next) }.isNotEmpty()
+    }
+}
+
+fun overlap1(fileName: String): Int {
     val test = readInput(fileName).count { pairs ->
         val elfRange = pairs.split(",").map {
                 range -> range.split("-").map { it.toInt() } }
--- src/test/kotlin/Day04KtTest.kt
+++ src/test/kotlin/Day04KtTest.kt
@@ -4,14 +4,26 @@ import org.junit.jupiter.api.Test
 class Day04KtTest {
 
     @Test
-    fun `Test sum of contains ranges`() {
+    fun `Test sum of contains ranges, small`() {
         val pairs = contains("Day04.txt")
         assertEquals(5, pairs)
     }
 
     @Test
-    fun `Test sum of overlap ranges`() {
+    fun `Test sum of contains ranges, all`() {
+        val pairs = contains("Day04_all.txt")
+        assertEquals(582, pairs)
+    }
+
+    @Test
+    fun `Test sum of overlap ranges, small`() {
         val pairs = overlap("Day04.txt")
         assertEquals(7, pairs)
+    }
+
+    @Test
+    fun `Test sum of overlap ranges, all`() {
+        val pairs = overlap("Day04_all.txt")
+        assertEquals(893, pairs)
     }
 }
